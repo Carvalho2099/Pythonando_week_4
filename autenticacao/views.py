1d6eb7f757bb9e7d433d31a5322bb735b057e222
Felipe Carvalho <31414765+Carvalho2099@users.noreply.github.com>
2022-07-07 17:47:24 -0300

    dia 2

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,6 +1,5 @@
 from string import hexdigits
 from django.shortcuts import render
-from django.http import HttpResponse
 from .utils import password_is_valid, send_email_html
 from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth.models import User
@@ -10,7 +9,6 @@ import os
 from django.conf import settings
 from .models import Ativacao
 from hashlib import sha256
-# from django.contrib import auth
 
 def cadastro(request):
     if request.method == "GET":
@@ -38,7 +36,7 @@ def cadastro(request):
 
             path_template = os.path.join(settings.BASE_DIR, 'autenticacao/templates/emails/cadastro_confirmado.html')
 
-            send_email_html(path_template, 'Cadastro confirmado', [email,], username=username)
+            send_email_html(path_template, 'Cadastro confirmado', [email,], username=username, link_ativacao=f"127.0.0.1:8000/auth/ativar_conta/{token}")
 
             messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso')
 
